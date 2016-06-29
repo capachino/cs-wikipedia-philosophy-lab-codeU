27484117d2438581a5de6ac9a43eab239afb1ce4
jyum <jyum@localhost>
2016-06-28 18:09:46 -0700

    Done.

--- javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
+++ javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
@@ -11,7 +11,7 @@ import org.jsoup.nodes.TextNode;
 import org.jsoup.select.Elements;
 
 public class WikiPhilosophy {
-	
+	final static List<String> visited = new ArrayList<String>();
 	final static WikiFetcher wf = new WikiFetcher();
 	
 	/**
@@ -29,23 +29,66 @@ public class WikiPhilosophy {
 	 */
 	public static void main(String[] args) throws IOException {
 		
-        // some example code to get you started
-
-		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
-		Elements paragraphs = wf.fetchWikipedia(url);
-
-		Element firstPara = paragraphs.get(0);
+		String destination = "https://en.wikipedia.org/wiki/Philosophy";
+		String source = "https://en.wikipedia.org/wiki/Java_(programming_language)";
 		
-		Iterable<Node> iter = new WikiNodeIterable(firstPara);
-		for (Node node: iter) {
-			if (node instanceof TextNode) {
-				System.out.print(node);
-			}
-        }
+		testConjecture(destination, source, 10);		
+	}
 
-        // the following throws an exception so the test fails
-        // until you update the code
-        String msg = "Complete this lab by adding your code and removing this statement.";
-        throw new UnsupportedOperationException(msg);
+	/**
+	 * Starts from given URL and follows first link until it finds the destination or exceeds the limit.
+	 * 
+	 * @param destination
+	 * @param source
+	 * @throws IOException
+	 */
+	public static void testConjecture(String destination, String source, int limit) throws IOException {
+		String url = source;
+		for (int i=0; i<limit; i++) {
+			if (visited.contains(url)) {
+				System.err.println("We're in a loop, exiting.");
+				return;
+			} else {
+				visited.add(url);
+			}
+			Element elt = getFirstValidLink(url);
+			if (elt == null) {
+				System.err.println("Got to a page with no valid links.");
+				return;
+			}
+			
+			System.out.println("**" + elt.text() + "**");
+			url = elt.attr("abs:href");
+			
+			if (url.equals(destination)) {
+				System.out.println("Eureka!");
+				break;
+			}
+		}
+	}
+
+	/**
+	 * Loads and parses a URL, then extracts the first link.
+	 * 
+	 * @param url
+	 * @return the Element of the first link, or null.
+	 * @throws IOException
+	 */
+	public static Element getFirstValidLink(String url) throws IOException {
+		print("Fetching %s...", url);
+		Elements paragraphs = wf.fetchWikipedia(url);
+		WikiParser wp = new WikiParser(paragraphs);
+		Element elt = wp.findFirstLink();
+		return elt;
+	}
+
+	/**
+	 * Formats and print the arguments.
+	 * 
+	 * @param msg
+	 * @param args
+	 */
+	private static void print(String msg, Object... args) {
+		System.out.println(String.format(msg, args));
 	}
 }
